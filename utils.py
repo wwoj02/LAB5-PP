ce6585cdb1ec99a6ffaa5599ebd1cdbd836f9d2d
Olik <Olivier.jakepl@gmail.com>
2025-06-06 08:44:03 +0200

    naprawka123

--- utils.py
+++ utils.py
@@ -1,12 +1,13 @@
-def add (a: int , b: int) -> int :
+def add(a: int, b: int) -> int:
     return a + b
 
-def subtract (a: int , b: int) -> int :
+def subtract(a: int, b: int) -> int:
     return a - b
 
-def multiply (a: int , b: int) -> int :
+def multiply(a: int, b: int) -> int:
     return a * b
 
-def divide ( a: int , b : int ) -> float :
+def divide(a: int, b: int) -> float:
+    if b == 0:
+        raise ValueError("Cannot divide by zero!")
     return a / b
-
