abeedb01eaf1f17c203b8d36279ec1e528d0cea2
wojte <wojtek0201@gmail.com>
2025-06-06 20:20:16 +0200

    poprawa bledow zad 4.9 Pylint

--- utils.py
+++ utils.py
@@ -1,13 +1,23 @@
+"""Zestaw podstawowych funkcji kalkulatora."""
+
+
 def add(a: int, b: int) -> int:
+    """suma a i b"""
     return a + b
 
+
 def subtract(a: int, b: int) -> int:
+    """roznica a i b"""
     return a - b
 
+
 def multiply(a: int, b: int) -> int:
+    """iloczyn a i b"""
     return a * b
 
+
 def divide(a: int, b: int) -> float:
+    """iloraz a i b jesli b == 0 podnosi wyjatek"""
     if b == 0:
         raise ValueError("Cannot divide by zero!")
     return a / b
